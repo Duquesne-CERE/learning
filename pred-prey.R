ea85cb4451e9e4523634dec4f6944ead00296357
David Kahler <dmkahler@users.noreply.github.com>
2020-09-17 20:17:47 -0400

    Add files via upload
    Added plot, adjusted time step and length of arrays to automatically calculate size based on target time

--- pred-prey.R
+++ pred-prey.R
@@ -1,10 +1,10 @@
 # Constants
-dt <- 0.1
-breed <- 0.8
-prey_rate <- 0.4
-pred_grow <- 0.5 * prey_rate
-pred_deat <- 0.4
-iter <- 1000
+dt <- 0.001
+breed <- 0.6
+prey_rate <- 0.3
+pred_grow <- 0.20 * prey_rate
+pred_deat <- 0.5
+iter <- round(100/dt)
 
 # Preallocation
 x <- array(-9999, dim = iter)
@@ -13,8 +13,8 @@ t <- x
 
 # Initial conditions
 t[1] <- 0
-x[1] <- 4
-y[1] <- 2
+x[1] <- 6 # initial prey
+y[1] <- 2 # initial pred
 
 # Actual calculations
 for (i in 2:iter) {
@@ -23,3 +23,5 @@ for (i in 2:iter) {
       y[i] <- y[(i-1)] + dt * (y[(i-1)] * x[(i-1)] * pred_grow - y[(i-1)] * pred_deat)
 }
 
+plot(t,x, type = "l", ylab = "prey population", xlab = "time")
+plot(t,y, type = "l", ylab = "predator population", xlab = "time")

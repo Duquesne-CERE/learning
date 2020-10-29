1198443f12b2d8c352cae0212f9cd3bb2a8cb83c
David Kahler <dmkahler@users.noreply.github.com>
2020-10-29 17:59:27 -0400

    Add files via upload

---
+++ evaporation.R
@@ -0,0 +1,4 @@
+ebbr <- read.table('cement_EBBR.csv', header = TRUE, sep = ",", dec = ".", stringsAsFactors = FALSE)
+pan <- read.table('chickasha_PAN.csv', header = TRUE, sep = ",", dec = ".", stringsAsFactors = FALSE)
+
+# set the start date with <- as.Date("1953-6-1")

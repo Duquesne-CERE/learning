367f58b1833e4533144291da6d8709663c65b2da
David Kahler <dmkahler@users.noreply.github.com>
2020-11-05 17:49:35 -0500

    Add files via upload
    Added loop for date

--- evaporation.R
+++ evaporation.R
@@ -1,4 +1,29 @@
-ebbr <- read.table('cement_EBBR.csv', header = TRUE, sep = ",", dec = ".", stringsAsFactors = FALSE)
-pan <- read.table('chickasha_PAN.csv', header = TRUE, sep = ",", dec = ".", stringsAsFactors = FALSE)
+library(dplyr)
+library(readr)
+library(lubridate)
+
+ebbr <- read.table('cement_EBBR.csv', header = TRUE, sep = ",", dec = ".", stringsAsFactors = FALSE) # W/m^2
+pan <- read.table('chickasha_PAN.csv', header = TRUE, sep = ",", dec = ".", stringsAsFactors = FALSE) # hundredths of inches
+
+actual_day <- ebbr$Day.Number # day 1 is 01 Jun 1993
+actual_mes <- ebbr$Latent.Heat.Flux..daytime..W.m.2.
 
 # set the start date with <- as.Date("1953-6-1")
+start <- as.Date("1953-6-1")
+end <- as.Date("2004-10-09")
+
+
+
+day <- array(0, dim = (end-start+1))
+for (i in 1:(end-start+1)) {
+      
+      day[i] <- start + i - 1
+      
+      dom <- mday(day[i])
+      mon <- month(day[i])
+      year <- year(day[i])
+      
+      match(pan$YEARMO,(100*year+month))
+      
+}
+

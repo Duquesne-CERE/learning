411725d09126376ce38734630d41f167bb7a9392
David Kahler <dmkahler@users.noreply.github.com>
2020-11-05 19:34:20 -0500

    Add files via upload
    Fixed mday, month, and year function

--- evaporation.R
+++ evaporation.R
@@ -9,8 +9,8 @@ actual_day <- ebbr$Day.Number # day 1 is 01 Jun 1993
 actual_mes <- ebbr$Latent.Heat.Flux..daytime..W.m.2.
 
 # set the start date with <- as.Date("1953-6-1")
-start <- as.Date("1953-6-1")
-end <- as.Date("2004-10-09")
+start <- as.Date("1953-6-1", origin = "1970-01-01")
+end <- as.Date("2004-10-09", origin = "1970-01-01")
 
 
 
@@ -19,9 +19,9 @@ for (i in 1:(end-start+1)) {
       
       day[i] <- start + i - 1
       
-      dom <- mday(day[i])
-      mon <- month(day[i])
-      year <- year(day[i])
+      dom <- mday(as.Date(day[i], origin = "1970-01-01"))
+      mon <- month(as.Date(day[i], origin = "1970-01-01"))
+      year <- year(as.Date(day[i], origin = "1970-01-01"))
       
       match(pan$YEARMO,(100*year+month))
       

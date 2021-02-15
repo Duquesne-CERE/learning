3237e2714792cef6e8b1eb985ef6df04ccd69437
David Kahler <dmkahler@users.noreply.github.com>
2021-02-15 16:36:40 -0500

    Add files via upload
    Logistic model from class 15 Feb 2021.  See the associated figures for comparison of numerical (this) versus analytical solutions.

---
+++ logistic.R
@@ -0,0 +1,23 @@
+# Code for the logistic growth model
+r <- 0.01 # growth rate
+K <- 80 # carrying capacity
+p0 <- 5 # inital population
+time <- 1000 # run <time> units
+dt <- 1 # time step
+
+# Logistic growth model:
+# dP/dt = r ((K-p)/K) P
+
+p <- array(-9, dim = c(time/dt))
+p[1] <- p0
+for (i in 1:(nrow(p)-1)) {
+      p[i+1] <- p[i] + (dt * (p[i] * r * (K-p[i]) / K))
+}
+
+plot(p, type = "p", col = "blue", xlim = c(0,time), xlab = "Time (units)", ylab = "population")
+
+# compare to the analytic solution:
+tim <- (c(1:(time/dt)))*dt
+pop <- K / (1+((K/p0)-1)*exp(-r*tim))
+lines(tim,pop)
+# Here, you see an example of truncation error, if you use a dt>>1 and rounding error with dt<<1.
